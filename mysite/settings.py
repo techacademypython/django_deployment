9d6a4d47eedbb635295f39ffdf06f14b23285c74
munisisazade <munisisazade@gmail.com>
2019-09-10 20:08:53 +0400

    database changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -71,12 +71,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': "deploy_db",
+            'USER': "deploy_db_user",
+            'PASSWORD': "Ak3rrnDaxX2ShX3t"
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
